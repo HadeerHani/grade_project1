637643f9938058edbc6a13f714ebcbff6aea00ee
elaraby <m.y.elaraby6@gmail.com>
2026-04-12 22:06:57 +0200

    ID verification & camira and gallary access

--- lib/screens/create_account_screen.dart
+++ lib/screens/create_account_screen.dart
@@ -1,11 +1,10 @@
-import 'dart:ffi' as ffi;
-
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart'; // مكتبة اختيار الصور
+import 'dart:io'; // للتعامل مع الملفات
 import 'package:second_project/screens/login_screen.dart';
 import 'package:second_project/screens/send_code_screen.dart';
 import 'package:second_project/screens/welcome_screen_modified.dart';
 
-//import 'package:second_project/screens/create_account_screen.dart';
 class CreateAccountScreen extends StatefulWidget {
   const CreateAccountScreen({super.key});
 
@@ -19,12 +18,17 @@ class _CreateAccountScreenState extends State<CreateAccountScreen> {
   final TextEditingController fullNameController = TextEditingController();
   final TextEditingController phoneController = TextEditingController();
   final TextEditingController passwordController = TextEditingController();
-  final TextEditingController confirmPasswordController = TextEditingController();
+  final TextEditingController confirmPasswordController =
+      TextEditingController();
   final TextEditingController ssnController = TextEditingController();
   final TextEditingController bioController = TextEditingController();
-  
+
   String? _selectedRole;
-  
+
+  // 1. تعريف متغير لحفظ الصورة والـ ImagePicker
+  XFile? _profileImage;
+  final ImagePicker _picker = ImagePicker();
+
   @override
   void dispose() {
     emailController.dispose();
@@ -37,14 +41,29 @@ class _CreateAccountScreenState extends State<CreateAccountScreen> {
     super.dispose();
   }
 
+  // 2. دالة اختيار الصورة من المعرض
+  Future<void> _pickImage() async {
+    try {
+      final XFile? pickedFile = await _picker.pickImage(
+        source: ImageSource.gallery,
+        imageQuality: 50, // لتقليل حجم الصورة
+      );
+      if (pickedFile != null) {
+        setState(() {
+          _profileImage = pickedFile;
+        });
+      }
+    } catch (e) {
+      debugPrint("Error picking image: $e");
+    }
+  }
+
   Widget buildRoleButton(String text, String role) {
     bool isSelected = _selectedRole == role;
-    IconData iconData;
-    if (role == 'Worker') {
-      iconData = Icons.build;
-    } else {
-      iconData = Icons.person_2_outlined;
-    }
+    IconData iconData = role == 'Worker'
+        ? Icons.build
+        : Icons.person_2_outlined;
+
     return Expanded(
       child: ElevatedButton.icon(
         icon: Icon(iconData, size: 20),
@@ -81,7 +100,6 @@ class _CreateAccountScreenState extends State<CreateAccountScreen> {
           child: Form(
             key: _formKey,
             child: Column(
-              spacing: 10,
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 const Text(
@@ -92,6 +110,7 @@ class _CreateAccountScreenState extends State<CreateAccountScreen> {
                     color: AppColors.primaryDarkGreen,
                   ),
                 ),
+                const SizedBox(height: 10),
                 const Text(
                   'Join Fixpay Today',
                   style: TextStyle(
@@ -99,117 +118,66 @@ class _CreateAccountScreenState extends State<CreateAccountScreen> {
                     color: AppColors.primaryDarkGreen,
                   ),
                 ),
+                const SizedBox(height: 20),
+                // خانات الإدخال الأساسية
                 TextFormField(
                   controller: fullNameController,
-                  decoration: InputDecoration(
+                  decoration: const InputDecoration(
                     hintText: 'Full Name',
                     prefixIcon: Icon(Icons.person_2_outlined),
-                    errorBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.red, width: 2),
-                    ),
-                    focusedErrorBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.red, width: 2),
-                    ),
                   ),
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Please enter your full name';
-                    }
-                    return null;
-                  },
+                  validator: (value) =>
+                      value!.isEmpty ? 'Please enter your full name' : null,
                 ),
+                const SizedBox(height: 10),
                 TextFormField(
                   controller: emailController,
-                  decoration: InputDecoration(
+                  decoration: const InputDecoration(
                     hintText: 'Email Address',
                     prefixIcon: Icon(Icons.email_outlined),
-                    errorBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.red, width: 2),
-                    ),
-                    focusedErrorBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.red, width: 2),
-                    ),
                   ),
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Please enter your email';
-                    }
-                    if (!value.contains('@')) {
-                      return 'Please enter a valid email';
-                    }
-                    return null;
-                  },
+                  validator: (value) => (value == null || !value.contains('@'))
+                      ? 'Please enter a valid email'
+                      : null,
                 ),
+                const SizedBox(height: 10),
                 TextFormField(
                   controller: phoneController,
                   keyboardType: TextInputType.phone,
-                  decoration: InputDecoration(
+                  decoration: const InputDecoration(
                     hintText: 'Phone Number',
                     prefixIcon: Icon(Icons.phone_android_outlined),
-                    errorBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.red, width: 2),
-                    ),
-                    focusedErrorBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.red, width: 2),
-                    ),
                   ),
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Please enter your phone number';
-                    }
-                    if (value.length < 10) {
-                      return 'Please enter a valid phone number';
-                    }
-                    return null;
-                  },
+                  validator: (value) => (value!.length < 10)
+                      ? 'Please enter a valid phone number'
+                      : null,
                 ),
+                const SizedBox(height: 10),
                 TextFormField(
                   controller: passwordController,
                   obscureText: true,
-                  decoration: InputDecoration(
+                  decoration: const InputDecoration(
                     hintText: 'Password',
                     prefixIcon: Icon(Icons.lock),
-                    errorBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.red, width: 2),
-                    ),
-                    focusedErrorBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.red, width: 2),
-                    ),
                   ),
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Please enter your password';
-                    }
-                    if (value.length < 6) {
-                      return 'Password must be at least 6 characters';
-                    }
-                    return null;
-                  },
+                  validator: (value) => (value!.length < 6)
+                      ? 'Password must be at least 6 characters'
+                      : null,
                 ),
+                const SizedBox(height: 10),
                 TextFormField(
                   controller: confirmPasswordController,
                   obscureText: true,
-                  decoration: InputDecoration(
+                  decoration: const InputDecoration(
                     hintText: 'Confirm Password',
                     prefixIcon: Icon(Icons.lock),
-                    errorBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.red, width: 2),
-                    ),
-                    focusedErrorBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.red, width: 2),
-                    ),
                   ),
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Please confirm your password';
-                    }
-                    if (value != passwordController.text) {
-                      return 'Passwords do not match';
-                    }
-                    return null;
-                  },
+                  validator: (value) => (value != passwordController.text)
+                      ? 'Passwords do not match'
+                      : null,
                 ),
-                Text(
+                const SizedBox(height: 20),
+                const Text(
                   'I am a :',
                   style: TextStyle(
                     color: AppColors.primaryDarkGreen,
@@ -226,43 +194,7 @@ class _CreateAccountScreenState extends State<CreateAccountScreen> {
                   ],
                 ),
 
-                if (_selectedRole == 'Customer')
-                  Padding(
-                    padding: const EdgeInsets.only(top: 25.0),
-                    child: ElevatedButton(
-                      onPressed: () {
-                        if (_formKey.currentState!.validate()) {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) {
-                                return VerifyAccountScreen(
-                                  email: emailController.text,
-                                  selectedRole: 'Customer',
-                                );
-                              },
-                            ),
-                          );
-                        }
-                      },
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: Color.fromARGB(255, 218, 208, 178),
-                        foregroundColor: AppColors.primaryDarkGreen,
-                        minimumSize: const Size(double.infinity, 50),
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(30.0),
-                        ),
-                      ),
-                      child: const Text(
-                        'Continue',
-                        style: TextStyle(
-                          fontSize: 18,
-                          color: AppColors.primaryDarkGreen,
-                        ),
-                      ),
-                    ),
-                  ),
-
+                // عرض الحقول الإضافية في حال اختيار Worker
                 if (_selectedRole == 'Worker') ...[
                   const SizedBox(height: 20),
                   TextFormField(
@@ -270,31 +202,25 @@ class _CreateAccountScreenState extends State<CreateAccountScreen> {
                     keyboardType: TextInputType.number,
                     maxLength: 9,
                     decoration: const InputDecoration(
-                      labelText: 'Social Security Number (Required for Worker)',
-                      hintText: '9-digit SSN (e.g., 123456789)',
+                      labelText: 'Social Security Number',
                       counterText: '',
-                      errorBorder: OutlineInputBorder(
-                        borderSide: BorderSide(color: Colors.red, width: 2),
-                      ),
-                      focusedErrorBorder: OutlineInputBorder(
-                        borderSide: BorderSide(color: Colors.red, width: 2),
-                      ),
                     ),
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return 'SSN is required for workers';
-                      }
-                      if (value.length != 9) {
-                        return 'SSN must be exactly 9 digits';
-                      }
-                      return null;
-                    },
+                    validator: (value) =>
+                        (value!.length != 9) ? 'SSN must be 9 digits' : null,
                   ),
                   const SizedBox(height: 20),
+
+                  // 3. تعديل زرار رفع الصورة ليظهر حالة الرفع أو المعاينة
                   ElevatedButton.icon(
-                    icon: const Icon(Icons.photo_camera_outlined),
-                    label: const Text('Upload Profile Photo (Optional)'),
-                    onPressed: () {},
+                    onPressed: _pickImage,
+                    icon: _profileImage == null
+                        ? const Icon(Icons.photo_camera_outlined)
+                        : const Icon(Icons.check_circle, color: Colors.green),
+                    label: Text(
+                      _profileImage == null
+                          ? 'Upload Profile Photo (Optional)'
+                          : 'Photo Selected ✅',
+                    ),
                     style: ElevatedButton.styleFrom(
                       minimumSize: const Size(double.infinity, 50),
                       backgroundColor: Colors.white70,
@@ -304,6 +230,24 @@ class _CreateAccountScreenState extends State<CreateAccountScreen> {
                       ),
                     ),
                   ),
+
+                  // معاينة الصورة المختارة (اختياري)
+                  if (_profileImage != null)
+                    Padding(
+                      padding: const EdgeInsets.only(top: 10),
+                      child: Center(
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(10),
+                          child: Image.file(
+                            File(_profileImage!.path),
+                            height: 100,
+                            width: 100,
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                      ),
+                    ),
+
                   const SizedBox(height: 20),
                   TextFormField(
                     controller: bioController,
@@ -313,65 +257,58 @@ class _CreateAccountScreenState extends State<CreateAccountScreen> {
                       hintText: 'e.g., Plumber with 10 years experience',
                     ),
                   ),
-                  const SizedBox(height: 25),
-                  ElevatedButton(
-                    onPressed: () {
-                      if (_formKey.currentState!.validate()) {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) {
-                              return VerifyAccountScreen(
-                                email: emailController.text,
-                                selectedRole: 'Worker',
-                              );
-                            },
-                          ),
-                        );
-                      }
-                    },
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: const Color.fromARGB(255, 218, 208, 178),
-                      foregroundColor: AppColors.primaryDarkGreen,
-                      minimumSize: const Size(double.infinity, 50),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(30.0),
-                      ),
-                    ),
-                    child: const Text(
-                      'Continue',
-                      style: TextStyle(
-                        fontSize: 18,
-                        color: AppColors.primaryDarkGreen,
-                      ),
-                    ),
-                  ),
-                  const SizedBox(height: 15),
                 ],
 
-                if (_selectedRole != 'Worker')
+                const SizedBox(height: 25),
+                // زر الاستمرار
+                ElevatedButton(
+                  onPressed: () {
+                    if (_selectedRole == null) {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        const SnackBar(content: Text('Please select a role')),
+                      );
+                      return;
+                    }
+                    if (_formKey.currentState!.validate()) {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => VerifyAccountScreen(
+                            email: emailController.text,
+                            selectedRole: _selectedRole!,
+                          ),
+                        ),
+                      );
+                    }
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: const Color.fromARGB(255, 218, 208, 178),
+                    foregroundColor: AppColors.primaryDarkGreen,
+                    minimumSize: const Size(double.infinity, 50),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(30.0),
+                    ),
+                  ),
+                  child: const Text('Continue', style: TextStyle(fontSize: 18)),
+                ),
+
+                if (_selectedRole != 'Worker') ...[
+                  const SizedBox(height: 15),
                   Center(
                     child: Row(
                       mainAxisAlignment: MainAxisAlignment.center,
                       children: [
                         const Text(
                           "Already have an account?",
-                          style: TextStyle(
-                            color: Color.fromRGBO(56, 94, 72, 1),
-                            fontSize: 19,
-                          ),
+                          style: TextStyle(fontSize: 19),
                         ),
                         TextButton(
-                          onPressed: () {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                builder: (context) {
-                                  return LoginScreen();
-                                },
-                              ),
-                            );
-                          },
+                          onPressed: () => Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => const LoginScreen(),
+                            ),
+                          ),
                           child: const Text(
                             'log in',
                             style: TextStyle(fontSize: 19),
@@ -380,6 +317,7 @@ class _CreateAccountScreenState extends State<CreateAccountScreen> {
                       ],
                     ),
                   ),
+                ],
               ],
             ),
           ),
